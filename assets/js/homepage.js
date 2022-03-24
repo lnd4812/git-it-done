b79a7a766f007900309ef61cb5ad742b26e18e25
LNDavid <ldavid@rmacan.com>
2022-03-23 21:06:16 -0400

    add a containers to display issues or alert if none

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -23,7 +23,7 @@ var formSubmitHandler = function(event) {
 
 var getUserRepos = function(user) {
   // format the github api url
-  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  var apiUrl = "https://api.github.com/users/" + user + " /repos";
  
   // make a request to the url
   fetch(apiUrl).then(function(response) {
